291b29056fdefbaf770b3957858ae6f2c925370a
将之大西 <omccello@gmail.com>
2021-11-18 00:52:23 +0900

    lec130

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
 import { Layout } from '../components/layout/layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
